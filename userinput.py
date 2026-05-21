efbc7e6012b689dbb425d4968d44301dd3b57a52
devi eswari <vanapallidevieswari@gmail.com>
2026-05-21 17:25:29 +0530

    Add files via upload

---
+++ userinput.py
@@ -0,0 +1,51 @@
+name = input("Enter the name: ")
+branch = input("Enter the branch: ")
+section = input("Enter the section: ")
+college_name = input("Enter the college name: ")
+print("Name of the student:", name)
+print("Branch:", branch)
+print("Section:", section)
+print("College Name:", college_name)
+#stuent details
+
+'''name = str(input("Enter the name: "))
+branch = str(input("Enter the branch: "))
+section = str(input("Enter the section: "))
+college_name = str(input("Enter the college name: "))
+print(f"Name of the student:{name}and Branch:{branch} and Section:{section} and College Name: {college_name}")'''
+'''o/p:Enter the name: devi
+Enter the branch: csm
+Enter the section: b
+Enter the college name: ideal
+Name of the student:deviand Branch:csm and Section:b and College Name: ideal'''
+
+
+#a=int(input("enter the value1:"))
+#b=int(input("enter the value2:"))
+#sum= a+b
+#print(sum)
+#add two numbers
+'''o/p:enter the value1:20
+enter the value2:30
+50'''
+
+
+'''l=int(input("enter the length:"))
+b=int(input("enter the breadth:"))
+area= l*b
+print(f"area of rectangle:{area}")
+o/p:enter the length:9
+enter the breadth:8
+area of rectangle:72'''
+
+
+'''P=int(input("enter the principle:"))
+T=int(input("enter the time:"))
+R=int(input("enter the rate:"))
+si= P*T*R/100
+print(f"simple interest is:{si}")
+
+o/p:enter the principle:10
+enter the time:20
+enter the rate:30
+simple interest is:60.0'''
