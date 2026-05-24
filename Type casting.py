07e986897bd0cdd639b37107c99734d837546718
devi eswari <vanapallidevieswari@gmail.com>
2026-05-24 16:54:23 +0530

    Add files via upload

---
+++ Type casting.py
@@ -0,0 +1,44 @@
+#Typecasting:
+#convert one form of datatype to another form datatype
+
+#Integer to string
+a=10
+b= str(a)
+print(type(a))
+print(type(b))
+o/p:<class 'int'>
+<class 'str'>
+
+#string to integer
+x="10"
+y=int(x)
+print(type(x))
+print(type(y))
+o/p:<class 'str'>
+<class 'int'>
+
+#integer to float
+a=10
+b=float(a)
+print(type(a))
+print(type(b))
+o/p:<class 'int'>
+<class 'float'>
+
+#integer to string
+c=50
+d=str(c)
+print(type(c))
+print(type(d))
+o/p:<class 'int'>
+<class 'str'>
+
+#float to integer
+e=70.0
+g=int(e)
+print(type(e))
+print(type(g))
+o/p:<class 'float'>
+<class 'int'>
+
+
